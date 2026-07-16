914d6f8d994e6de5cef4d025936788286a3d5a90
marie kanno <marie.abe0306@gmail.com>
2026-07-16 11:07:08 +0900

    feat: レビュー機能実装 #5

---
+++ app/Http/Requests/StoreReviewRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'rating' => 'required|integer|in:1,2,3,4,5',
+            'comment' => 'nullable|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'rating.required' => '評価は必須です。',
+            'rating.integer' => '評価は整数で入力してください。',
+            'rating.in' => '評価は1～5で入力してください。',
+            'comment.string' => 'コメントは文字列で入力してください。',
+        ];
+    }
+}
