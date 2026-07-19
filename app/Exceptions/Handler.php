98e500f628fd308974edb17d0c7d45998939ec14
marie kanno <marie.abe0306@gmail.com>
2026-07-19 11:31:11 +0900

    API例外ハンドリングを実装

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +27,18 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (
+            NotFoundHttpException $e,
+            Request $request
+        ) {
+            if (
+                $request->is('api/v1/books/*')) {
+                return response()->json([
+                    'message' => '書籍が見つかりませんでした。',
+                ], 404);
+            }
         });
     }
 }
